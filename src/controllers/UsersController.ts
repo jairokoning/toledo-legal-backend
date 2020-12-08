948789f2a848e872ca1dab885c6b69aa3393d86e
Jairo Koning <jairo.koning@gmail.com>
2020-12-08 06:14:46 -0300

    Feature: Users (Controller, Model)

---
+++ src/controllers/UsersController.ts
@@ -0,0 +1,122 @@
+import {request, Request, Response} from 'express';
+import { getRepository } from  'typeorm';
+import { hash } from 'bcryptjs';
+import * as Yup from 'yup';
+
+import User from '../models/User';
+
+export default {
+  async index(request: Request, response: Response) {
+    const usersRepository = getRepository(User);
+
+    const users = await usersRepository.find();
+
+    return response.status(200).json(users);
+  },
+
+  async show(request: Request, response: Response) {
+    const { id } = request.params;
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOneOrFail(id);
+
+    return response.status(200).json(user);
+  },
+
+  async create(request: Request, response: Response) {    
+    const { name, email, password, password_confirmation } = request.body;
+
+    const usersRepository = getRepository(User);    
+
+    const data = { name, email, password, password_confirmation };
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required('Nome é obrigatório'),
+      email: Yup.string().email('Informe um e-mail válido').required('E-mail é obrigatório'),
+      password: Yup.string().min(6, 'No mínimo 6 dígitos').required('Senha é obrigatório'),
+      password_confirmation: Yup
+        .string()
+        .oneOf(
+          [Yup.ref('password'), undefined],
+          'Confirmação incorreta')
+        .required('Confirmação da senha é obrigatório'),
+    })
+
+    await schema.validate(data, {
+      abortEarly: false,
+    });
+
+    const hashedPassword = await hash(password, 8);
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await usersRepository.save(user);
+
+    return response.status(201).json(user);
+  },
+
+  async update(request: Request, response: Response) {
+    try {
+      const { id } =request.params;
+      const { name, email, password, password_confirmation } = request.body;
+
+      const usersRepository = getRepository(User);   
+
+      const data = { name, email, password, password_confirmation };
+
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome é obrigatório'),
+        email: Yup.string().email('Informe um e-mail válido').required('E-mail é obrigatório'),
+        password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+        password_confirmation: Yup
+          .string()
+          .oneOf(
+            [Yup.ref('password'), undefined],
+            'Confirmação incorreta')        
+      })
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      if (password && password_confirmation) {
+        data.password = await hash(password, 8);
+      }
+
+      const user = await usersRepository.findOneOrFail(id);
+
+      user.name = data.name;
+      user.email = data.email;
+      user.password = data.password;
+
+      usersRepository.create(user);
+
+      await usersRepository.save(user);
+
+      return response.status(201).json(user);
+    } catch (error) {
+      return response.status(500).json({ message: 'Internal server error' });
+    }
+  },
+
+  async destroy(request: Request, response: Response) {
+    try {
+      const { id } = request.params;
+
+      const usersRepository = getRepository(User);
+
+      const user = await usersRepository.findOneOrFail(id);
+
+      await usersRepository.remove(user);
+
+      return response.status(204).json();
+    } catch (error) {
+      return response.status(500).json({ message: 'Internal server error' });
+    }
+    
+  }
+};
--- src/routes.ts
+++ src/routes.ts
@@ -2,10 +2,17 @@ import { Router } from 'express';
 import multer from 'multer';
 
 import uploadConfig from './config/upload';
+import UsersController from './controllers/UsersController';
 import OrphanagesController from './controllers/OrphanagesController';
 
 const routes = Router();
 const upload = multer(uploadConfig);
+
+routes.get('/users', UsersController.index);
+routes.get('/users/:id', UsersController.show);
+routes.post('/users', UsersController.create);
+routes.put('/users/:id', UsersController.update);
+routes.delete('/users/:id', UsersController.destroy);
 
 routes.get('/orphanages', OrphanagesController.index);
 routes.get('/orphanages/:id', OrphanagesController.show);
